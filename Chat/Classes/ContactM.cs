0896a6faaefaa869df9311ff064f8436b391d8a3
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-03-26 20:38:07 +0300

    2 Classes, accepting value

---
+++ Chat/Classes/ContactM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Chat.Classes
+{
+    class ContactM
+    {
+        public string Username { get; set; }
+        public string ImageSource { get; set; }
+        public ObservableCollection<MessageM> Messages{  get; set; }
+        public string LastMessage => Messages.Last().Message;
+    }
+}
